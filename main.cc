1c6b4376fd7965e1289eb69074a7c0fdfe881859
wanjuncong <Magicl7@outlook.com>
2024-02-24 11:26:42 +0800

    update:练习目录清理干净

--- main.cc
+++ main.cc
@@ -1,34 +1,76 @@
-// C++ Program to evaluate largest of the three numbers
-// using nested if else
+/**
+ * @file main.cc
+ * @author wanjuncong (Magcil7@outlook.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-02-24
+ * 
+ * @copyright Copyright (c) WANJUNCONG 2024
+ * 
+ */
+
 #include <iostream>
+#include <fstream>
+#include <string>
+
 using namespace std;
   
+/**
+ * @brief 
+ * 
+ * @return int 
+ */
 int main()
 {
+    /*  */
+    /* ┎─────────────────────────────────────────────────────────────────┒ */
+    
+    /* ┗─────────────────────────────────────────────────────────────────┚ */
+
+    
+    /* 测试打开文件 */
+    /* ┎─────────────────────────────────────────────────────────────────┒ */
+    // std::string filePath = R"(data\subdata\subsubdata\sub_log.txt)";
+    // std::ifstream file(filePath);
+
+    // if (file.is_open()) {
+    //     std::string line;
+    //     while (std::getline(file, line)) {
+    //         std::cout << line << std::endl;
+    //     }
+    //     file.close();
+    // } else {
+    //     std::cout << "无法打开文件" << std::endl;
+    // }
+    /* ┗─────────────────────────────────────────────────────────────────┚ */
+
+    /* 不知道什么 */
+    /* ┎─────────────────────────────────────────────────────────────────┒ */
     // declaring three numbers
-    int a = 10;
-    int b = 2;
-    int c = 6;
+    // int a = 10;
+    // int b = 2;
+    // int c = 6;
   
-    // outermost if else
-    if (a < b) {
-        // nested if else
-        if (c < b) {
-            printf("%d is the greatest", b);
-        }
-        else {
-            printf("%d is the greatest", c);
-        }
-    }
-    else {
-        // nested if else
-        if (c < a) {
-            printf("%d is the greatest", a);
-        }
-        else {
-            printf("%d is the greatest", c);
-        }
-    }
+    // // outermost if else
+    // if (a < b) {
+    //     // nested if else
+    //     if (c < b) {
+    //         printf("%d is the greatest", b);
+    //     }
+    //     else {
+    //         printf("%d is the greatest", c);
+    //     }
+    // }
+    // else {
+    //     // nested if else
+    //     if (c < a) {
+    //         printf("%d is the greatest", a);
+    //     }
+    //     else {
+    //         printf("%d is the greatest", c);
+    //     }
+    // }
+    /* ┗─────────────────────────────────────────────────────────────────┚ */
   
     return 0;
 }
