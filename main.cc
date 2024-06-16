ea684f44f1d6a7219384feb60481797f5d9d0b1f
MirrorStr <Magicl7@outlook.com>
2024-06-16 11:24:03 +0800

    add:增加json相关内容的测试

--- main.cc
+++ main.cc
@@ -15,10 +15,10 @@
 #include <vector>
 #include <sstream>
 #include <windows.h>
+#include <fstream>
 
 #include "json/json.h"
 
-// #include "Head.hh"
 #include "Api.hh"
 
 using namespace std;
@@ -30,7 +30,138 @@ using namespace std;
  */
 int main()
 {
+
     #if 1
+    /* json文件读写 */
+    /* ┎─────────────────────────────────────────────────────────────────┒ */
+    /* 打开一个输出文件，没有会创建 */
+    std::ofstream sOutFile;
+    std::string strOutPath("D:/Project/C++/data/");
+    try
+    {
+        sOutFile.open(strOutPath + "data.json");
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        return -1;
+    }
+    /* 准备好json内容 */
+    Json::Value jsRoot;
+    jsRoot["Meb1"] = "Hello";
+    jsRoot["Meb2"] = 7;
+    jsRoot["Meb3"] = true;
+    /* 写入然后关闭 */
+    sOutFile << jsRoot << std::endl;
+    sOutFile.close();
+
+    /* +=+=+=+=+= 读取文件 +=+=+=+=+= */
+    std::string strReadPath("D:/Project/C++/data/DataRead.json");
+    std::ifstream sFileRead(strReadPath);
+#if 1
+    /* 使用C风格的报错 */
+    if (!sFileRead) {   /* 打开是否成功 */
+        std::cout << "Can't open " << strReadPath << ", Error:" << strerror(errno) << "."  << std::endl;
+        return -1; 
+    }
+#endif
+    Json::Reader jsReader;
+    Json::Value jsReadRoot;
+#if 0
+    /* 不用异常处理来解析 */
+    if (jsReader.parse(sFileRead, jsReadRoot)) {    /* 解析成json对象 */
+    std::cout << "jsReadRoot:" << jsReadRoot << std::endl;
+    } else {
+        std::cout << "Parse error" << std::endl;
+    }
+#else
+    try
+    {
+        if (jsReader.parse(sFileRead, jsReadRoot)) {    /* 解析成json对象 */
+            std::cout << "jsReadRoot:" << jsReadRoot << std::endl;
+        } else {
+            throw Json::Exception("Failed to parse jsReadRoot!");
+        }
+    }
+    catch(const Json::Exception &e)
+    {
+        std::cerr << "Catch a json::exp with mesg: " << e.what() << '\n';
+    }
+#endif
+    
+
+    /* 结论： 写入很简单，打开输出流之后当作标准输出一样直接'<<'就完事了，都不用转string。二，对于输入，利用reader对象从文件流中
+       解析除json对象然后就是随便操作了。
+    */
+    /* ┗─────────────────────────────────────────────────────────────────┚ */
+    #endif
+
+    #if 0
+    /* json member测试 */
+    /* ┎─────────────────────────────────────────────────────────────────┒ */
+    Json::Value jsRoot;
+    jsRoot["Meb1"] = "Hello";
+    jsRoot["Meb2"] = 7;
+    jsRoot["Meb3"] = true;
+    Json::Value jsSub;
+    jsSub["Head"] = 1;
+    jsSub["Foot"] = 2;
+    Json::Value jsSubArr;
+    for (int i = 0; i < 3; i++) {
+        Json::Value jsEle;  /* 数组元素 */
+        jsEle["Ele"] = i;
+        jsSubArr.append(jsEle);
+    }
+    jsSub["Arr"] = jsSubArr;
+    jsRoot["MultiMeb"] = jsSub;
+    auto Mebs = jsRoot.getMemberNames();    /* Root对象的所有成员 */
+    std::cout << "--------------" << std::endl;
+    for (auto Meb : Mebs) {
+        std::cout << "- " << Meb << std::endl;
+    }
+
+    Mebs = jsSub.getMemberNames();  /* 子对象的所有成员 */
+    std::cout << "--------------" << std::endl;
+    for (auto Meb : Mebs) {
+        std::cout << "- " << Meb << std::endl;
+    }
+
+    // 数组不能使用
+    // Mebs = jsSubArr.getMemberNames();
+    // std::cout << "--------------" << std::endl;
+    // for (auto Meb : Mebs) {
+    //     std::cout << "- " << Meb << std::endl;
+    // }
+
+    std::cout << "Root:\n" << jsRoot.toStyledString() << std::endl;
+
+    /* 查看对象的大小，而不是数组 */
+    std::cout << "jsRoot size:" << jsRoot.size() << std::endl;
+
+    /* 使用find() */
+    std::string strFind = "Meb1";
+    Json::Value jsFind = jsRoot.find(strFind.data(), strFind.data() + strFind.size());
+    std::cout << "jsFind1:" << jsFind.toStyledString() << std::endl;
+
+    std::string strFind2 = "MMBB";
+    Json::Value jsFind2 = jsRoot.find(strFind2.data(), strFind2.data() + strFind2.size());
+    std::cout << "jsFind2:" << jsFind2.toStyledString() << std::endl;
+
+    auto itJson = jsRoot.begin();
+    std::cout << "\nitJson:" << (*itJson).toStyledString() << std::endl;
+
+
+    std::cout << "\n Print json arr" << std::endl;
+    for (auto Meb : jsSubArr) {
+        std::cout << "- [Meb Ele]: " << Meb["Ele"].asInt() << std::endl;
+        std::cout << "- " << Meb << std::endl;
+    }
+    std::cout << "Root:" << jsRoot << std::endl;
+    /* 结论： */
+    /* ┗─────────────────────────────────────────────────────────────────┚ */
+    #endif
+
+    #if 0
     /* 测试Json库 */
     /* ┎─────────────────────────────────────────────────────────────────┒ */
     /**
